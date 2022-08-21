815449afa46e0eff116aba993860e903fab18e4a
jofrhwld <jofrhwld@gmail.com>
2022-08-21 18:26:28 -0400

    rendering

--- render.sh
+++ render.sh
@@ -1,4 +1,4 @@
 #quarto render notation.qmd --execute --output index.html
-#quarto render notation.qmd --execute --to pdf
 quarto render notation.qmd --execute --to hugo --output _index.md
+quarto render notation.qmd --execute --to pdf
 
